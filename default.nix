6277c00e305109ba0f7249cc346c9731164eeb0b
Eduardo Asafe <e@asafe.dev>
2021-11-13 22:18:37 +0800

    Set up reflex

---
+++ default.nix
@@ -0,0 +1,23 @@
+{ system ? builtins.currentSystem
+, obelisk ? import ./.obelisk/impl {
+    inherit system;
+    iosSdkVersion = "13.2";
+
+    # You must accept the Android Software Development Kit License Agreement at
+    # https://developer.android.com/studio/terms in order to build Android apps.
+    # Uncomment and set this to `true` to indicate your acceptance:
+    config.android_sdk.accept_license = true;
+
+    # In order to use Let's Encrypt for HTTPS deployments you must accept
+    # their terms of service at https://letsencrypt.org/repository/.
+    # Uncomment and set this to `true` to indicate your acceptance:
+    # terms.security.acme.acceptTerms = false;
+  }
+}:
+with obelisk;
+project ./. ({ ... }: {
+  android.applicationId = "systems.typestruck.hundred.before";
+  android.displayName = "100before";
+  ios.bundleIdentifier = "systems.typestruck.hundred.before";
+  ios.bundleName = "100before";
+})
--- loader/server.js
+++
@@ -1,1 +0,0 @@
-require('../output/Server.Main').main();
