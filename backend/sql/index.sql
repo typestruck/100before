90e7a5ab48855379d960342ea1c42e5c14b6df0f
Eduardo Asafe <e@asafe.dev>
2022-01-15 16:57:03 -0300

    Initial table structures

---
+++ backend/sql/index.sql
@@ -0,0 +1,24 @@
+create table users (
+    id integer primary key generated always as identity,
+    name text not null,
+    avatar text not null,
+    description text not null,
+    birthday timestamptz,
+    email text not null,
+    password text not null
+);
+
+create table todos (
+    id integer primary key generated always as identity,
+    createdBy integer not null references users(id),
+    deadline text not null
+);
+
+create table todo_items (
+    id integer primary key generated always as identity,
+    todo integer not null references todos(id),
+    short_description text not null,
+    long_description text,
+    status smallint not null,
+    public boolean not null
+);
